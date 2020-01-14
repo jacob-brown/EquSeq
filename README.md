c6664e4e3f5b6d05638e3bfc76f5b239bdf445f3
Jacob Brown <jacob.42.brown@gmail.com>
2020-01-14 14:23:48 +0000

    modern and ancient bioproject numbers gathered

---
+++ README.md
@@ -0,0 +1,54 @@
+# Horse Genomics
+
+
+## Running To Do 
+* List all datasets (bioproject codes)
+* script to find data associated with bioproject codes
+* script to filter any unusuable data, summary table of data types and sample size
+* Can other data be gathered from the list of all bioproject codes?
+
+
+
+## Workflow
+### Overview
+If BAM files are used the pipline and methods should be the same, as otherwise we won't be able to compare the sequences. 
+
+FASTQ would allow us to use our own mapping and pipeline, however will be alot more work. 
+
+For the writeup we should aim for a hypothesis driven approach. We use the sample and want to find out the ancestry, mixture, etc. Rather than a methods write-up.
+
+### Steps
+**1.Available Data**
+* Look at Orlando
+* Collect all available data (everything!)
+* What sort of format is it in? FASTQ, BAM?
+* What pipeline/protocols where used? are they comparable?
+* Pipeline and protocol should be comparable. 
+* Where is it from?
+
+**1.1 Ancient DNA (extra)**
+* needs to be anlysied seperatly from the modern, and should be done after the gathering of the modern samples. 
+* Common issues: Less coverage, potential contamination, DNA damage, transition/transversion tends to be an issue, centre of fragments is usually good but edges are not. 
+* When and where is it from?
+
+**2.Pipeline/Protocol**
+* from Orlando - how was the data processed?
+Mapping softwares - samtools etc. 
+
+**3. Analysis**
+* AMSD, NGSTOOLS, etc. 
+* Admixture plots, PCAs, trees, etc. 
+* lots can be done. 
+
+
+## Files
+
+### Code
+* `expand_projects.py` - expand the project codes and prepare list for a deeper search.
+
+### Data
+
+* `SraRunTable.txt` lists all the metadata from the following NCBI query: `(((horse[Organism]) OR Equus[Organism]) AND genomic[Source]) AND WGS[Strategy]`
+* `SRR_Acc_List.txt` is a list of the `Accession Codes`
+* `prj_modern.csv` - bioproject codes for modern sequences
+* `prj_ancient.csv` - bioproject codes for ancient sequences
---
+++ code/expand_projects.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Jacob Brown
+# Email j.brown19@imperial.ac.uk
+# Date:   2020-01-14
+# Last Modified: 2020-01-14
+
+
+
+""" Read in project codes and expand adding from suplementary table
+ 	check to ensure there are no duplicates, etc."""
+
+###########################################
+################# Modules #################
+###########################################
+
+import csv
+import numpy as np
+from copy import deepcopy as dc
+
+###########################################
+######### Input(s) and Parameters #########
+###########################################
+
+####################
+### project list ###
+
+project_list = [] # initialise the list
+
+with open('../data/papers_projects.csv', 'r') as file:
+	reader = csv.reader(file)
+	for row in reader:
+		project_list.append(row) # add row to list
+
+##################################
+### secondary list of projects ###
+
+loc_orlando = '../data{}'.format(project_list[1][-2]) # location of orlando bioproject codes for Origins paper
+
+project_orlando_all = [] # all data
+project_orlando = [] # just the project codes
+
+with open(loc_orlando, 'r') as file:
+	reader = csv.reader(file)
+	for row in reader:
+		project_orlando_all.append(row) # all data
+		project_orlando.append(row[3]) # project code
+
+###########################################
+############### Wraggling #################
+###########################################
+
+project_orlando = np.unique(project_orlando) # unique codes only
+project_orlando = np.delete(project_orlando, -1) # strip header element
+
+##############################################
+### list of metadata and new project codes ###
+
+list_to_append = []
+tmp = dc(project_list[1]) # temp object deep copy - prevent overwrite of project_list
+
+for i in project_orlando:
+	tmp[1] = i # update the project code whilst maintainng other metadata
+	list_to_append.append(tmp[:]) # append, slice to prevent shallow copy
+
+####################################
+### update the all projects list ###
+
+project_list = project_list + list_to_append # append newly generated list
+del project_list[1] # remove blank element that has now been populated
+
+with open('../data/project_list_update.csv', 'w') as file:
+	writer = csv.writer(file, delimiter=',')
+	for i in project_list:
+		writer.writerow(i)
+
+############################
+### Check for duplicates ###
+	# they are present. not an issues, as mutiple sources may have been used across papers
+
+all_project = [i[1] for i in project_list] # list of BioProject codes
+all_project = all_project[1:] # remove header
+all_project.sort()
+
+len(all_project) == len(np.unique(all_project)) # duplicates are present
+
+####################################################
+### Group project numbers by modern and ancient ####
+	# 'both' has ancient and modern genomes associated with the code
+
+# modern
+modern = [i[1] for i in project_list if i[2] =='modern' or i[2] == 'both'] # modern project codes
+
+modern = np.unique(modern)
+np.savetxt('../data/prj_modern.csv', modern, fmt='%s', delimiter=',') # save to csv
+
+
+# ancient
+ancient = [i[1] for i in project_list if i[2] =='ancient' or i[2] == 'both'] # ancient project codes
+
+ancient = np.unique(ancient)
+np.savetxt('../data/prj_ancient.csv', ancient, fmt='%s', delimiter=',')
+
+
+
+
