5d9fe8595741dd7c6176dbddbe2a390ccb691e77
Jacob Brown <jacob.42.brown@gmail.com>
2020-05-06 10:30:13 +0100

    snp selector now parses options, splits by chrom number

--- ancestry/selectSites.py
+++ ancestry/selectSites.py
@@ -3,11 +3,12 @@
 # Author: Jacob Brown
 # Email j.brown19@imperial.ac.uk
 # Date:   2020-05-04
-# Last Modified: 2020-05-05
+# Last Modified: 2020-05-06
 
 
 
-""" Select sites to be used for PCA """
+""" Generate sites for use in 
+	genotype liklihood and PCA  """
 
 ###########################################
 ################# Modules #################
@@ -19,37 +20,48 @@ import numpy as np
 import time
 from natsort import natsorted
 import argparse
+import pandas as pd 
+
 
 ###########################################
 ################# Options #################
 ###########################################
 
-#parser = argparse.ArgumentParser(description=\
-#		'Generate snp position list on a fuzzy position of a bam file.')
-#
-## input bam file
-#parser.add_argument("-i", "--input", dest="inbam", type=str,
-#					required=True, help="bam IN_BAM to have snps sampled from", \
-#					metavar="IN_BAM")
-#
-##  output directory
-#parser.add_argument("-o", "--out", dest="ourDir", type=str,
-#                  required=False, help="directory of output files")
-#
-## list output  name
+parser = argparse.ArgumentParser(description=\
+		'Generate snp position list on a fuzzy position of a bam file.')
+
+
+# command 
+choose = ["snps", "subsample", "split"]
+parser.add_argument("-c", "--command", dest="command", type=str,
+					required=True, choices=choose, 
+					help="choice function. snps: generate pickle of all sites; subsample: "\
+						"the sites; chrmSplit: split outfiles by chromosome.")
+
+# input bam file
+parser.add_argument("-i", "--input", dest="infile", type=str,
+					required=True, help="bam IN_BAM to have snps sampled from", \
+					metavar="IN_BAM")
+
+#  output directory
+parser.add_argument("-o", "--out", dest="outdir", type=str,
+                  required=False, default="./", help="directory of output files")
+
+# list output  name
 #parser.add_argument("-l", "--list", dest="outlist", type=str,
-#                  required=True, help="write list to OUT_LIST", metavar="OUT_LIST")
-#
-## base difference in snp position
-#parser.add_argument("-d", "--diff", dest="baseDiff", type=int,
-#                  required=True, help="int of number of bases between snps")
-#
-#
-#parser.add_argument("-n", "--num", dest="snpCount", type=int,
-#                  required=False, help="number of snps to return")
-#
-## define args
-#args = parser.parse_args()
+#                  required=False, help="write list to OUT_LIST", metavar="OUT_LIST")
+
+# base difference in snp position
+parser.add_argument("-d", "--diff", dest="baseDiff", type=int,
+                  required=False, default=10000, help="int of number of bases between snps")
+
+
+parser.add_argument("-n", "--num", dest="snpCount", type=int,
+                  required=False, default=500, help="number of snps to return")
+
+# define args
+args = parser.parse_args()
+
 
 
 ###########################################
@@ -115,6 +127,8 @@ def snpList(bamIn, pickleOut, baseDiff):
 	pickle.dump(fuzzyName, f)
 	f.close()
 
+
+
 def subSample(pickleIn, listOut, snpCount):
 	
 	""" return a subsampled list of length: snpCount """
@@ -126,26 +140,59 @@ def subSample(pickleIn, listOut, snpCount):
 	np.random.seed(12345)
 	subsample = np.random.choice(snps, snpCount, replace=False)
 	subsample = natsorted(subsample)
+	
 	saveTxt(listOut, subsample)
 
 
+def chrmSep(snpList, outDir):
+	""" split snp lists by chromosome """
+	f = open(snpList, "r")
+	snps = f.read().splitlines()
 
-###########################################
-######### Input(s) and Parameters #########
-###########################################
+	snps_split = [i.split(":") for i in snps]
+	df = pd.DataFrame(snps_split)
+	grouped = df.groupby(0)[1].apply(list)
+	df_grouped = pd.DataFrame(grouped)
+
+
+	# save files #
+	for index, row in df_grouped.iterrows():
+		outname = outDir + "." + row.name + ".list"
+		content = [row.name + ":" + i for i in row[1]]
+		saveTxt(outname, content)
+	
+	return 0
 
 
 ###########################################
 ############### Wraggling #################
 ###########################################
+# python ancestry/selectSites.py -c snps -i data/processed_sequences/new.rg.bam -o data/ancestry/snp.all -d 10000
 
-#snpList(bamIn= "data/processed_sequences/new.rg.bam", \
-#			pickleOut="data/ancestry/snp.all.p", baseDiff=10000)
+# python ancestry/selectSites.py -c subsample -i data/ancestry/snp.all.p -o data/ancestry/snp -n 5000
+
+# python ancestry/selectSites.py -c split -i data/ancestry/snp.list -o data/ancestry/snp.chr/snp
+
+if args.command == "snps":
+	### generate pickle file ###
+	snpList(bamIn=args.infile, \
+		pickleOut=args.outdir + ".p", baseDiff=args.baseDiff)
+
+elif args.command == "subsample":
+
+	#### subsample ###
+	subSample(pickleIn=args.infile, \
+		listOut=args.outdir+".list", snpCount=args.snpCount)
+
+elif args.command == "split":
+	### split chromosomes ###
+	chrmSep(snpList=args.infile, outDir=args.outdir)
+
+else:
+	print("not found")
 
 
-### subsample again ###
-##### ALTER AS NEEDED ######
-subSample(pickleIn="data/ancestry/snp.all.p", listOut="data/ancestry/snp.list", snpCount=50000)
+
 
 
 
--- sync.sh
+++ sync.sh
@@ -2,10 +2,15 @@
 # Author: Jacob Brown
 # Email j.brown19@imperial.ac.uk
 # Date:   2020-05-05
-# Last Modified: 2020-05-05
+# Last Modified: 2020-05-06
 # Desc: sync local files with those on HPC ignoring those in gitignore 
 
 LOCAL_DIR=./
 REMOTE_DIR=jb1919@login.cx1.hpc.ic.ac.uk:/rds/general/user/jb1919/home/genomics/HorseGenomics/
 
-rsync -av --delete --exclude-from='.gitignore' --exclude='.git/' $LOCAL_DIR $REMOTE_DIR
+rsync -av --delete --include='data/ancestry/*' \
+		--exclude-from='.gitignore' \
+		--exclude='.git/' \
+		$LOCAL_DIR $REMOTE_DIR
+
+
